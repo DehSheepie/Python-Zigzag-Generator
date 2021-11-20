dbbbefe54551021048dc4f41f04444657e8451e8
DehSheepie <shep@cyberdragon.online>
2021-11-20 15:21:02 +0000

    Redid the graph generation function

--- gen.py
+++ gen.py
@@ -15,15 +15,15 @@ class Graph:
 
     HEX = "0123456789abcdef"
 
-    def __init__(self, seed: str = "ffff", density=5, zagginess=5):
+    def __init__(self, seed: str = "ffff", x_nodes=5, y_nodes=5):
         self.__seed = seed
         self.__nodes = []
-        self.__density = density
-        self.__zagginess = zagginess
+        self.x_nodes = x_nodes
+        self.y_nodes = y_nodes
         self.gen_graph()
 
     def get_value(self, index, count):
-        num = Graph.HEX.index(self.__seed[index]) + 1
+        num = Graph.HEX.index(self.__seed[index])
 
         return ((num * count) % 0xf) + 1
 
@@ -31,24 +31,15 @@ class Graph:
         return self.__nodes
 
     def gen_graph(self) -> [(int, int)]:
-        x = MAX_WIDTH // 2
+        x_seg_size = MAX_WIDTH // self.x_nodes
+        y_seg_size = MAX_HEIGHT // self.y_nodes
         y = MAX_HEIGHT
-        y_seg = MAX_HEIGHT // self.__density  # segment size
-        y_variance = (y_seg // 0xf) + 1  # prevent 0 values
-        x_seg = MAX_WIDTH // self.__zagginess  # segment size
-        x_variance = (x_seg // 0xf) + 1
-        counter = 1
-        flip = False
+        x= MAX_WIDTH//2
         while True:
+            self.__nodes.append((x, y))
+            x = random.randint(0, self.x_nodes) * x_seg_size
             if y == 0:
                 break
-            y -= y_variance
+            y -= y_seg_size
             if y < 0:
                 y = 0
-            if flip:
-                x -= x_seg
-            else:
-                x += x_seg
-            if x > MAX_WIDTH - 20 or x < 20:
-                flip = not flip
-            self.__nodes.append((x, y))
